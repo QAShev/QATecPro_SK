0aa306a88ab90a6e2f4a800cffb73eccbf68db68
Kateryna Shevchenko <k.shevchenko@viseven.com>
2025-05-08 21:37:16 +0300

    main + hwpart1

--- lesson3_cycles&functions/lesson3_hw1_cycles.js
+++ lesson3_cycles&functions/lesson3_hw1_cycles.js
@@ -6,6 +6,12 @@ for (let i = 0; i < 8; i++) {
   console.log( `число - ` + i);
 }
 
+let i = 0;
+while (i < 8) {
+  console.log(`число - ` + i);
+  i++;
+}
+
 /* Task - 2
 
   Реалізувати алгоритм, який обчислює суму чисел від firstNumber до secondNumber (включно)
@@ -14,6 +20,13 @@ for (let i = 0; i < 8; i++) {
 
 let firstNumber = -100;
 let secondNumber = 250;
+
+let sum = 0;
+for (let i = firstNumber; i <= secondNumber; i++) {
+  sum += i;
+}
+
+console.log("The sum of numbers from " + firstNumber + " to " + secondNumber + " (including) = " + sum);
 
 /* Task - 3
 
@@ -27,14 +40,43 @@ let secondNumber = 250;
   8 seconds left 
 */
 
+let timeLeft = 10;
+
+let timer = setInterval(function() {
+  if (timeLeft > 0) {
+    console.log(`${timeLeft} seconds left`);
+    timeLeft--;
+  } else {
+    console.log('Time is up!');
+    clearInterval(timer); 
+  }
+}, 1000);
+
 /* Task - 4
 
   Скласти програму для виведення на екран наступної послідовності: 7 14 21 28 35 42 49 */
+
+  for (let i = 7; i <= 49; i += 7) {
+    console.log(i);
+  }
 
 /* Task - 5
 
   Напишіть функцію printMultiples, яка приймає число number і виводить усі кратні числа від 1 до 10 (включно) для цього числа за допомогою циклу do...while. 
   Наприклад, для number = 3 виведіть 3, 6, 9.*/
+
+  function printMultiples(number) {
+    let i = 1;
+    do {
+      let multiple = number * i;
+      if (multiple <= 10) {
+        console.log(multiple);
+      }
+      i++;
+    } while (number * i <= 10);
+  }
+  
+  printMultiples(3);
 
 /* Task - 6
 
@@ -43,6 +85,17 @@ let secondNumber = 250;
 
   what is factorial: https://www.mathsisfun.com/numbers/factorial.html 
 */
+
+function factorialFor(n) {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+console.log(factorialFor(5));
+
 
 /* Task - 7 
 
@@ -57,3 +110,12 @@ let secondNumber = 250;
 let bucket = 5; // Місткість одного відра
 let initialBathState = 0; // Початкове значення кількості води у ванній кімнаті
 let bathCapacity = 60; // Місткість ванни
+
+let bucketCount = 0;
+
+while (initialBathState + bucket <= bathCapacity) {
+  initialBathState += bucket; 
+  bucketCount++; 
+}
+
+console.log(`We need to ${bucketCount} buckets, to fill the tub to the level of ${initialBathState} liters.`);
