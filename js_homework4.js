c0b729c7d5e97a5bccf30c02f9d0d0fd232bc1d8
Kateryna Shevchenko <k.shevchenko@viseven.com>
2025-05-25 20:51:34 +0300

    unction checkProbabilityTheory

--- js_homework4.js
+++ js_homework4.js
@@ -1,1 +1,24 @@
-test file
+function checkProbabilityTheory(count) {
+    let evens = 0;
+    let odds = 0;
+
+    for (let i = 0; i < count; i++) {
+        const num = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
+        if (num % 2 === 0) {
+            evens++;
+        } else {
+            odds++;
+        }
+    }
+
+    const evenPercentage = ((evens / count) * 100).toFixed(2);
+    const oddPercentage = ((odds / count) * 100).toFixed(2);
+
+    console.log(`Кількість згенерованих чисел: ${count}`);
+    console.log(`Парних чисел: ${evens}`);
+    console.log(`Непарних чисел: ${odds}`);
+    console.log(`Відсоток парних: ${evenPercentage}%`);
+    console.log(`Відсоток непарних: ${oddPercentage}%`);
+}
+
+checkProbabilityTheory(10);
